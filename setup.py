5e990850e6fe9ba72e319f41bdfebaed94bf270a
erikvw <ew2789@gmail.com>
2018-08-07 19:58:34 -0500

    setup, install_requires, requirements, PEP8

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ setup(
     install_requires=[
         'edc-base',
         'edc-lab',
-        'edc_visit_schedule',
+        'edc-visit-schedule',
     ],
     classifiers=[
         'Environment :: Web Environment',
